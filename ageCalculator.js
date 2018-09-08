d04771df070be817bd6f0e3061f80058aa7caa57
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2018-09-08 21:00:15 +0200

    Add files via upload
    age calculator and name printout

---
+++ ageCalculator.js
@@ -0,0 +1,7 @@
+//Age calculator
+var yourAgeString = prompt("Please tell me how old are you");
+var yourAge = parseInt(yourAgeString);
+var daysAlive = yourAge * 365;
+console.log("You have been alive for " + daysAlive + " days");
+alert("You have been alive for " + daysAlive + " days");
+
---
+++ variables.js
@@ -0,0 +1,7 @@
+let firstName = prompt("What is your first name?");
+let lastName = prompt("What is your last name?");
+let age = prompt("How old are you?");
+
+console.log("So you are telling me, that your full name is " + firstName + " " + lastName + "?");
+
+console.log("You are " + age + " years old?");
